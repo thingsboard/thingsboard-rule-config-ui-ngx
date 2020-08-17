69413229583dd46cb6377d3ecbb88cfd34a40cf7
Igor Kulikov <ikulikov@thingsboard.io>
2020-08-17 11:52:26 +0300

    Revert to Angular 9 for runtime compiler compatibility

--- builders/static-serve/index.js
+++ builders/static-serve/index.js
@@ -9,7 +9,6 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createServer = exports.execute = void 0;
 const architect_1 = require("@angular-devkit/architect");
 const express = require("express");
 const http = require("http");
