772fb5a7a979e9225c11f627a5545253c57e45c2
Vladyslav_Prykhodko <vprykhodko@thingsboard.io>
2021-03-31 17:30:06 +0300

    UI: change from "Latest Timeseries" to "Timeseries Key" in the originator timeseries rule node; Change not correct show hint

--- projects/rulenode-core-config/src/lib/locale/rulenode-core-locale.constant.ts
+++ projects/rulenode-core-config/src/lib/locale/rulenode-core-locale.constant.ts
@@ -58,6 +58,7 @@ export default function addRuleNodeCoreLocaleEnglish(translate: TranslateService
           'notify-device': 'Notify Device',
           'notify-device-hint': 'If the message arrives from the device, we will push it back to the device by default.',
           'latest-timeseries': 'Latest timeseries',
+          'timeseries-key': 'Timeseries key',
           'data-keys': 'Message data',
           'metadata-keys': 'Message metadata',
           'relations-query': 'Relations query',
@@ -381,7 +382,7 @@ export default function addRuleNodeCoreLocaleEnglish(translate: TranslateService
           'get-latest-value-with-ts': 'Fetch Latest telemetry with Timestamp',
           'get-latest-value-with-ts-hint':
             'If selected, latest telemetry values will be added to the outbound message metadata with timestamp, ' +
-            'e.g: "temp": "&lcub;\\"ts\\":1574329385897,\\"value\\":42&rcub;"',
+            'e.g: "temp": "{"ts":1574329385897, "value":42}"',
           'use-redis-queue': 'Use redis queue for message persistence',
           'trim-redis-queue': 'Trim redis queue',
           'redis-queue-max-size': 'Redis queue max size',
