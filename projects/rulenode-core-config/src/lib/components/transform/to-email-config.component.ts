67f252fdfa6e57435b85ad6808debcaab54d59fb
Vladyslav_Prykhodko <vprykhodko@thingsboard.io>
2021-07-14 11:59:00 +0300

    Refactoring to email rule node and fix translation

--- projects/rulenode-core-config/src/lib/components/transform/to-email-config.component.ts
+++ projects/rulenode-core-config/src/lib/components/transform/to-email-config.component.ts
@@ -3,6 +3,7 @@ import { AppState } from '@core/public-api';
 import { RuleNodeConfiguration, RuleNodeConfigurationComponent } from '@shared/public-api';
 import { Store } from '@ngrx/store';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'tb-transformation-node-to-email-config',
@@ -12,7 +13,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class ToEmailConfigComponent extends RuleNodeConfigurationComponent {
 
   toEmailConfigForm: FormGroup;
-  selectorArray;
+  mailBodyTypes = [
+    {name: 'tb.mail-body-type.plain-text', value: 'false'},
+    {name: 'tb.mail-body-type.html', value: 'true'},
+    {name: 'tb.mail-body-type.dynamic', value: 'dynamic'}
+  ];
 
   constructor(protected store: Store<AppState>,
               private fb: FormBuilder) {
@@ -24,20 +29,27 @@ export class ToEmailConfigComponent extends RuleNodeConfigurationComponent {
   }
 
   protected onConfigurationSet(configuration: RuleNodeConfiguration) {
-    this.selectorArray = [{name:'Plaim Text',value: false}, {name:'HTML', value: true}, {name:'Dynamic',value: 'dynamic'}];
     this.toEmailConfigForm = this.fb.group({
       fromTemplate: [configuration ? configuration.fromTemplate : null, [Validators.required]],
       toTemplate: [configuration ? configuration.toTemplate : null, [Validators.required]],
       ccTemplate: [configuration ? configuration.ccTemplate : null, []],
       bccTemplate: [configuration ? configuration.bccTemplate : null, []],
       subjectTemplate: [configuration ? configuration.subjectTemplate : null, [Validators.required]],
-      mailBodyType: [configuration ? configuration.subjectTemplate : null],
+      mailBodyType: [configuration ? configuration.mailBodyType : null],
       isHtmlTemplate: [configuration ? configuration.isHtmlTemplate : null],
       bodyTemplate: [configuration ? configuration.bodyTemplate : null, [Validators.required]],
     });
 
-    this.toEmailConfigForm.get('mailBodyType').valueChanges.subscribe(() => {
-        this.toEmailConfigForm.get('isHtmlTemplate').patchValue('',{emitEvent:false});
+    this.toEmailConfigForm.get('mailBodyType').valueChanges.pipe(
+      startWith([configuration?.subjectTemplate])
+    ).subscribe((mailBodyType) => {
+      if (mailBodyType === 'dynamic') {
+        this.toEmailConfigForm.get('isHtmlTemplate').patchValue('', {emitEvent: false});
+        this.toEmailConfigForm.get('isHtmlTemplate').setValidators(Validators.required);
+      } else {
+        this.toEmailConfigForm.get('isHtmlTemplate').clearValidators();
+      }
+      this.toEmailConfigForm.get('isHtmlTemplate').updateValueAndValidity();
     });
   }
 }
