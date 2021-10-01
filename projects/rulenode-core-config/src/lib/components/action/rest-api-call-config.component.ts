66297dc148c9aa8a9ae76bcf3e0cc2e6de845f85
Matt Magoffin <git+matt@msqr.us>
2021-10-02 10:20:04 +1300

    Add ignoreRequestBody configuration option, to go along with thingsboard/thingsboard#5304.

--- projects/rulenode-core-config/src/lib/components/action/rest-api-call-config.component.ts
+++ projects/rulenode-core-config/src/lib/components/action/rest-api-call-config.component.ts
@@ -32,6 +32,7 @@ export class RestApiCallConfigComponent extends RuleNodeConfigurationComponent {
       restEndpointUrlPattern: [configuration ? configuration.restEndpointUrlPattern : null, [Validators.required]],
       requestMethod: [configuration ? configuration.requestMethod : null, [Validators.required]],
       useSimpleClientHttpFactory: [configuration ? configuration.useSimpleClientHttpFactory : false, []],
+      ignoreRequestBody: [configuration ? configuration.ignoreRequestBody : false, []],
       enableProxy: [configuration ? configuration.enableProxy : false, []],
       useSystemProxyProperties: [configuration ? configuration.enableProxy : false, []],
       proxyScheme: [configuration ? configuration.proxyHost : null, []],
