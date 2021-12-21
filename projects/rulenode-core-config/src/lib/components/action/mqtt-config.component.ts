bbdcfe784eef2ccf093aea47711effdf13a269d3
Chantsova Ekaterina <chantsova.ekaterina@gmail.com>
2021-12-21 18:23:21 +0200

    MQTT node: Client ID suffix parameter added

--- projects/rulenode-core-config/src/lib/components/action/mqtt-config.component.ts
+++ projects/rulenode-core-config/src/lib/components/action/mqtt-config.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
-import { AppState } from '@core/public-api';
+import { Component, OnDestroy } from '@angular/core';
+import { AppState, isNotEmptyStr } from '@core/public-api';
 import { RuleNodeConfiguration, RuleNodeConfigurationComponent } from '@shared/public-api';
 import { Store } from '@ngrx/store';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'tb-action-node-mqtt-config',
   templateUrl: './mqtt-config.component.html',
   styleUrls: ['./mqtt-config.component.scss']
 })
-export class MqttConfigComponent extends RuleNodeConfigurationComponent {
+export class MqttConfigComponent extends RuleNodeConfigurationComponent implements OnDestroy {
 
   mqttConfigForm: FormGroup;
+
+  private subscriptions: Subscription[] = [];
 
   constructor(protected store: Store<AppState>,
               private fb: FormBuilder) {
@@ -30,9 +33,27 @@ export class MqttConfigComponent extends RuleNodeConfigurationComponent {
       connectTimeoutSec: [configuration ? configuration.connectTimeoutSec : null,
         [Validators.required, Validators.min(1), Validators.max(200)]],
       clientId: [configuration ? configuration.clientId : null, []],
+      appendClientIdSuffix: [{
+        value: configuration ? configuration.appendClientIdSuffix : false,
+        disabled: !(configuration && isNotEmptyStr(configuration.clientId))
+      }, []],
       cleanSession: [configuration ? configuration.cleanSession : false, []],
       ssl: [configuration ? configuration.ssl : false, []],
       credentials: [configuration ? configuration.credentials : null, []]
     });
+
+    this.subscriptions.push(
+      this.mqttConfigForm.get('clientId').valueChanges.subscribe((clientIdValue) => {
+        if (isNotEmptyStr(clientIdValue)) {
+          this.mqttConfigForm.get('appendClientIdSuffix').enable({emitEvent: false});
+        } else {
+          this.mqttConfigForm.get('appendClientIdSuffix').disable({emitEvent: false});
+        }
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(s => s.unsubscribe());
   }
 }
