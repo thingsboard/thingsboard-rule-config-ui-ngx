3fd3f43c161baf01b43305bfc81dd147c867616e
Igor Kulikov <ikulikov@thingsboard.io>
2022-01-12 19:33:15 +0200

    Fix attributes config node ui. Update yarn.lock.

--- projects/rulenode-core-config/src/lib/components/action/attributes-config.component.ts
+++ projects/rulenode-core-config/src/lib/components/action/attributes-config.component.ts
@@ -28,7 +28,7 @@ export class AttributesConfigComponent extends RuleNodeConfigurationComponent {
   protected onConfigurationSet(configuration: RuleNodeConfiguration) {
     this.attributesConfigForm = this.fb.group({
       scope: [configuration ? configuration.scope : null, [Validators.required]],
-      notifyDevice: [configuration ? configuration.scope : true, []]
+      notifyDevice: [configuration ? configuration.notifyDevice : true, []]
     });
   }
 
