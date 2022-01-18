c639fc3ee5384946c194e66617baa7f016830a57
Igor Kulikov <ikulikov@thingsboard.io>
2022-01-18 17:50:13 +0200

    Add SystemJS bundling

--- static.serve.conf.js
+++ static.serve.conf.js
@@ -1,9 +1,9 @@
 const STATIC_SERVE_CONFIG = {
   '/static/rulenode/rulenode-core-config.js': {
-    'target': 'dist/rulenode-core-config/fesm2015/rulenode-core-config.js'
+    'target': 'dist/rulenode-core-config/system/rulenode-core-config.js'
   },
   '/static/rulenode/rulenode-core-config.js.map': {
-    'target': 'dist/rulenode-core-config/fesm2015/rulenode-core-config.js.map'
+    'target': 'dist/rulenode-core-config/system/rulenode-core-config.js.map'
   }
 }
 
