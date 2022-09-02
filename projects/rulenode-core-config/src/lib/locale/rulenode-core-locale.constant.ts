5db4c653efcc21f624b0cc2481c01c93008bffc0
Kalutka Zhenya <ykalytka@thingsboard.io>
2022-09-02 12:34:46 +0300

    Update change originator logic

--- projects/rulenode-core-config/src/lib/locale/rulenode-core-locale.constant.ts
+++ projects/rulenode-core-config/src/lib/locale/rulenode-core-locale.constant.ts
@@ -88,6 +88,7 @@ export default function addRuleNodeCoreLocaleEnglish(translate: TranslateService
           'originator-tenant': 'Tenant',
           'originator-related': 'Related',
           'originator-alarm-originator': 'Alarm Originator',
+          'originator-entity': 'Entity',
           'clone-message': 'Clone message',
           transform: 'Transform',
           'default-ttl': 'Default TTL in seconds',
