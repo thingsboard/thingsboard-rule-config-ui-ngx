c0a92406e21a37ec1c687d64867251be402bc340
Kalutka Zhenya <ykalytka@thingsboard.io>
2022-11-24 18:15:29 +0200

    Refactoring

--- projects/rulenode-core-config/src/lib/components/action/attributes-config.component.ts
+++ projects/rulenode-core-config/src/lib/components/action/attributes-config.component.ts
@@ -11,6 +11,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class AttributesConfigComponent extends RuleNodeConfigurationComponent {
 
+  attributeScopeMap = AttributeScope;
   attributeScopes = Object.keys(AttributeScope);
   telemetryTypeTranslationsMap = telemetryTypeTranslations;
 
