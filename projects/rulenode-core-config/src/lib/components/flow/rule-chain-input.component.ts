3aaca4b315fcd88974c7af010db9c91e01fa5e0a
Kalutka Zhenya <ykalytka@thingsboard.io>
2023-01-11 14:22:02 +0200

    Update UI for rule-chaine-input node

--- projects/rulenode-core-config/src/lib/components/flow/rule-chain-input.component.ts
+++ projects/rulenode-core-config/src/lib/components/flow/rule-chain-input.component.ts
@@ -1,19 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AppState } from '@core/public-api';
 import { EntityType, RuleNodeConfiguration, RuleNodeConfigurationComponent } from '@shared/public-api';
 import { Store } from '@ngrx/store';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'tb-flow-node-rule-chain-input-config',
   templateUrl: './rule-chain-input.component.html',
   styleUrls: []
 })
-export class RuleChainInputComponent extends RuleNodeConfigurationComponent {
+export class RuleChainInputComponent extends RuleNodeConfigurationComponent implements OnDestroy {
 
   entityType = EntityType;
 
   ruleChainInputConfigForm: FormGroup;
+
+  private destroy$ = new Subject();
 
   constructor(protected store: Store<AppState>,
               private fb: FormBuilder) {
@@ -26,8 +30,24 @@ export class RuleChainInputComponent extends RuleNodeConfigurationComponent {
 
   protected onConfigurationSet(configuration: RuleNodeConfiguration) {
     this.ruleChainInputConfigForm = this.fb.group({
-      ruleChainId: [configuration ? configuration.ruleChainId : null, [Validators.required]]
+      transferMsgToOriginatorRootRuleChain: [configuration ? configuration.transferMsgToOriginatorRootRuleChain : null,[]],
+      ruleChainId: [configuration ? configuration.ruleChainId : false, [Validators.required]]
     });
+
+    this.ruleChainInputConfigForm.get('transferMsgToOriginatorRootRuleChain').valueChanges.pipe(
+      takeUntil(this.destroy$)
+    ).subscribe((value) => {
+      if (value) {
+        this.ruleChainInputConfigForm.get('ruleChainId').disable({emitEvent: false});
+        this.ruleChainInputConfigForm.get('ruleChainId').patchValue(null, {emitEvent: false});
+      } else {
+        this.ruleChainInputConfigForm.get('ruleChainId').enable({emitEvent: false});
+      }
+    })
   }
 
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
--- projects/rulenode-core-config/src/lib/locale/rulenode-core-locale.constant.ts
+++ projects/rulenode-core-config/src/lib/locale/rulenode-core-locale.constant.ts
@@ -479,6 +479,7 @@ export default function addRuleNodeCoreLocaleEnglish(translate: TranslateService
             'for value from metadata, <code><span style="color: #000;">$[</span>messageKey<span style="color: #000;">]</span></code> ' +
             'for value from message body to substitute "Source" and "Target" key names',
           'shared-scope': 'Shared scope',
+          'transfer-msg-to-originator': 'Transfer message to originator',
           'server-scope': 'Server scope',
           'client-scope': 'Client scope',
           'attribute-type': 'Attribute',
