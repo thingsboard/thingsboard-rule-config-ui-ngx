0270609556b7696dd28fd9b1a9b4fb2cd970d30a
Ivan Raznatovskyi <iraznatovskyi@thingsboard.io>
2023-09-06 11:42:32 +0300

    Added styles, rules and murkup for 'Rules chain' -> transformation -> 'Change-originator' and 'Copy-keys'

--- projects/rulenode-core-config/src/lib/components/transform/copy-keys-config.component.ts
+++ projects/rulenode-core-config/src/lib/components/transform/copy-keys-config.component.ts
@@ -9,7 +9,7 @@ import { COMMA, ENTER, SEMICOLON } from '@angular/cdk/keycodes';
 @Component({
   selector: 'tb-transformation-node-copy-keys-config',
   templateUrl: './copy-keys-config.component.html',
-  styleUrls: []
+  styleUrls: ['../../../../style.scss']
 })
 
 export class CopyKeysConfigComponent extends RuleNodeConfigurationComponent{
