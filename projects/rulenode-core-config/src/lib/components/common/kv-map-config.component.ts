2576230057d8e64c3ee17654c9c38ca3e23f90ad
Ivan Raznatovskyi <iraznatovskyi@thingsboard.io>
2023-12-05 11:54:32 +0200

    Merged wit master

--- projects/rulenode-core-config/src/lib/components/common/kv-map-config.component.ts
+++ projects/rulenode-core-config/src/lib/components/common/kv-map-config.component.ts
@@ -13,9 +13,11 @@ import {
   ValidatorFn,
   Validators
 } from '@angular/forms';
-import { coerceBoolean } from '@shared/public-api';
-import { isEqual } from '@core/public-api';
+import { coerceBoolean, PageComponent } from '@shared/public-api';
+import { Store } from '@ngrx/store';
+import { AppState, isEqual } from '@core/public-api';
 import { Subject, takeUntil } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'tb-kv-map-config',
