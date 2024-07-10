dbd82a3098ad4302fc3c2ea06b6fff8df52ec335
Artem Dzhereleiko <Dgerik2015@gmail.com>
2024-07-10 17:19:13 +0300

    UI: Update save time series node

--- projects/rulenode-core-config/src/lib/components/action/timeseries-config.component.ts
+++ projects/rulenode-core-config/src/lib/components/action/timeseries-config.component.ts
@@ -7,7 +7,7 @@ import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms
 @Component({
   selector: 'tb-action-node-timeseries-config',
   templateUrl: './timeseries-config.component.html',
-  styleUrls: []
+  styleUrls: ['../../../../style.scss']
 })
 export class TimeseriesConfigComponent extends RuleNodeConfigurationComponent {
 
